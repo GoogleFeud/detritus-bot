9391f7335cc0d81254d0b1a97c236c2d1b41f280
GoogleFeud <volen.sl666@gmail.com>
2021-10-01 10:35:18 +0300

    Basic bot

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
 
-console.log("Hello World!");
+import dotenv from "dotenv";
+import {ShardClient} from "detritus-client";
+dotenv.config();
+
+(() => {
+    const shardClient = new ShardClient(process.env.TOKEN as string, {
+        cache: {
+            sessions: false,
+            messages: false,
+            channels: false,
+            relationships: false,
+            roles: false,
+            stageInstances: false,
+            stickers: false,
+            voiceCalls: false,
+            voiceConnections: false,
+            voiceStates: false
+        }
+    });
+
+    //const commandClient = new Detritus.InteractionCommandClient(shardClient);
+
+    shardClient.on("messageCreate", (payload) => {
+        console.log(payload.message.content);
+    });
+
+    shardClient.run();
+})();
+
+
